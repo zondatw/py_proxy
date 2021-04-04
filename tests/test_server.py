96d4e74f0fd0221386e8daf04dc9ab6b7d8dceee
zonda yang <u226699@gmail.com>
2021-04-04 17:04:48 +0800

    Add test: ServerTest

---
+++ tests/test_server.py
@@ -0,0 +1,45 @@
+import socket
+import unittest
+from unittest.mock import Mock, patch
+
+from zoxy.server import ProxyServer
+
+class ServerTest(unittest.TestCase):
+    def setUp(self):
+        self.config = {
+            "url": "0.0.0.0",
+            "port": 9999,
+        }
+        self.proxy_server = ProxyServer(**self.config)
+
+    def tearDown(self):
+        self.proxy_server.close()
+
+    @patch("socket.socket", return_value=Mock())
+    def test_get_dest_socket(self, mock_socket):
+        testee = ("127.0.0.1", 8000)
+        dest_socket = self.proxy_server.get_dest_socket(*testee)
+        mock_socket.assert_called_with(socket.AF_INET, socket.SOCK_STREAM)
+        dest_socket.connect.assert_called_with(testee)
+        dest_socket.settimeout.assert_called_with(self.proxy_server._ProxyServer__dest_connection_timeout)
+
+    @patch("zoxy.server.ProxyServer.pipe_data", return_value=None)
+    def test_pipe(self, mock_pipe_data):
+        mock_src_socket = Mock()
+        mock_dest_socket = Mock()
+        test_request = b"Test requests\r\n"
+
+        # is_https_tunnel: False
+        self.proxy_server.pipe(mock_src_socket, test_request, mock_dest_socket, False)
+        self.assertEqual(mock_src_socket.sendall.call_count, 0)
+        mock_dest_socket.sendall.assert_called_with(test_request)
+        mock_pipe_data.assert_called_with(mock_src_socket, mock_dest_socket)
+
+        mock_src_socket.reset_mock()
+        mock_dest_socket.reset_mock()
+        mock_pipe_data.reset_mock()
+        # is_https_tunnel: True
+        self.proxy_server.pipe(mock_src_socket, test_request, mock_dest_socket, True)
+        self.assertEqual(mock_dest_socket.sendall.call_count, 0)
+        mock_src_socket.sendall.assert_called_with(b"HTTP/1.1 200 Connection established\r\n\r\n")
+        mock_pipe_data.assert_called_with(mock_src_socket, mock_dest_socket)
