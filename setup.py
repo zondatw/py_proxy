505c9e38efc9a6ecc7d5529ac5a8c5ab1797c1b8
zonda yang <u226699@gmail.com>
2021-04-07 16:48:33 +0800

    Bumpversion 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="zoxy",
-    version="0.0.3",
+    version="0.0.4",
     author="Zonda Yang",
     author_email="u226699@gmail.com",
     description="python proxy",
